34f1c11dcf6d94a999d581057023d18b064f75e5
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-27 21:40:06 -0600

    added to api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://jobly-backend-7rbk.onrender.com";
 
 class JoblyApi {
   static token;
